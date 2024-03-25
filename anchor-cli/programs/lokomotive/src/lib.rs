8744977afa335cd3af4706ec6b559fc650b6105c
Virjilakrum <asesenep15@gmail.com>
2024-03-26 02:01:12 +0300

    Wormhole bridge ve create Solana wallet

--- anchor-cli/programs/lokomotive/src/lib.rs
+++ anchor-cli/programs/lokomotive/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-// Wormhole SDK kütüphaneleri
+// Wormhole SDK Library
 use wormhole_sdk::{bridge::Bridge, solana::SolanaWallet, token::Token, types::*};
 
 declare_id!("Fhip5iGYbNmDmzB9H4DUiXXEhotfzCfgqajk95Fs7FCK");
@@ -9,32 +9,13 @@ declare_id!("Fhip5iGYbNmDmzB9H4DUiXXEhotfzCfgqajk95Fs7FCK");
 pub mod multichain_transfer {
     use super::*;
 
-    pub fn share_documents(
-        ctx: Context<Leaks>,
-        title: String,
-        description: String,
-        docs: String,
-    ) -> Result<()> {
-        let files: &mut Account<Files> = &mut ctx.accounts.files;
-        files.title = title;
-        files.description = description;
-        files.docs = docs;
-        Ok(())
-    }
-}
+/// Function: Solana to Ethereum
 
-#[derive(Accounts)]
-pub struct Leaks<'info> {
-    #[account(init,payer=user,space=64*3)]
-    pub files: Account<'info, Files>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[account]
-pub struct Files {
-    pub title: String,
-    pub description: String,
-    pub docs: String,
-}
+        pub fn transfer_sol_to_eth(
+            ctx: Context<MultichainTransfer>,
+            token_address: Pubkey,
+            amount: u64,
+        ) -> Result<()> {
+            // Wormhole bridge ve create Solana wallet
+            let bridge = Bridge::new(ctx.accounts.bridge.to_account_info());
+            let solana_wallet = SolanaWallet::new(ctx.accounts.solana_wallet.to_account_info());
