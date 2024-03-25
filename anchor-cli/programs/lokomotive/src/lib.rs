648df6d8c104e46e0bcf36de1072b93ed52e7f7c
Virjilakrum <asesenep15@gmail.com>
2024-03-26 01:58:42 +0300

    pub mod

--- anchor-cli/programs/lokomotive/src/lib.rs
+++ anchor-cli/programs/lokomotive/src/lib.rs
@@ -1,12 +1,21 @@
 use anchor_lang::prelude::*;
 
+// Wormhole SDK kütüphaneleri
+use wormhole_sdk::{bridge::Bridge, solana::SolanaWallet, token::Token, types::*};
+
 declare_id!("Fhip5iGYbNmDmzB9H4DUiXXEhotfzCfgqajk95Fs7FCK");
 
 #[program]
-pub mod solana_file_sharing_system {
+pub mod multichain_transfer {
     use super::*;
-    pub fn share_documents(ctx:Context<Leaks>,title:String,description:String,docs:String) -> Result<()> {
-        let files : &mut Account<Files> =  &mut ctx.accounts.files;
+
+    pub fn share_documents(
+        ctx: Context<Leaks>,
+        title: String,
+        description: String,
+        docs: String,
+    ) -> Result<()> {
+        let files: &mut Account<Files> = &mut ctx.accounts.files;
         files.title = title;
         files.description = description;
         files.docs = docs;
@@ -17,15 +26,15 @@ pub mod solana_file_sharing_system {
 #[derive(Accounts)]
 pub struct Leaks<'info> {
     #[account(init,payer=user,space=64*3)]
-    pub files: Account<'info,Files>,
+    pub files: Account<'info, Files>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info,System>
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct Files{
-    pub title : String, 
-    pub description : String,
-    pub docs : String,
+pub struct Files {
+    pub title: String,
+    pub description: String,
+    pub docs: String,
 }
